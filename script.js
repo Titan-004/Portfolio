31b449b5445803c9c2a082cc593ad547100f1a12
Titan-004 <136785642+Titan-004@users.noreply.github.com>
2023-10-24 20:57:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+gsap.from("header .logo" , {
+    y:10,
+    opacity:0,
+    delay:0.2,
+    duration:0.5,
+    stagger:0.2
+
+})
+gsap.from(" section .home-content h1" , {
+    y:20,
+    opacity:0,
+    delay:0.4,
+    duration:0.5,
+    stagger:0.1
+
+})
+gsap.from(" section .home-content h3" , {
+    y:20,
+    opacity:0,
+    delay:0.9,
+    duration:0.5,
+    stagger:0.1
+
+})
